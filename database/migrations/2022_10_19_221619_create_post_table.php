3b607d6caac79bd7f4afdf7ffd8f8b2ccaf9dce8
DaanRos <daan17@ziggo.nl>
2022-11-01 10:42:17 +0100

    Second view working + broken database

--- database/migrations/2022_10_19_221619_create_post_table.php
+++ database/migrations/2022_10_19_221619_create_post_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->date('date');
+            $table->text('text');
+            $table->id('userid');
+            $table->text('imglink');
+
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'show']);
+Route::get('test', [\App\Http\Controllers\test::class, 'show']);
